1161ca9da95e5375c9542a3f5f86ace9280180f9
Nicole Mors <nicolemors@gmail.com>
2020-10-10 10:14:17 -0700

    tokens + typography

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,8 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+      <section>
+        <h1>Page title</h1>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
